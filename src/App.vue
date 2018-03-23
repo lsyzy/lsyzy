d7ccdd43519f104b4112585fe6303e294066f759
lsy <875650086@qq.com>
2018-03-23 12:00:16 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="meg" @click="handleClick">{{message}}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      message: 'hello world'
+    }
+  },
+  methods: {
+    handleClick () {
+      this.message = 'bye world'
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .meg {
+    color: red;
+  }
 </style>
